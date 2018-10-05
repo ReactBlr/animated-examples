5f108508989d9e9708dc4840a86c44e9f6ab0a60
Kashish Grover <kashish.grover@treebohotels.com>
2018-10-06 01:25:35 +0530

    add spinner animation

--- App.js
+++ App.js
@@ -3,15 +3,76 @@ import {
   StyleSheet,
   Text,
   View,
+  Animated,
+  Easing,
 } from 'react-native';
 
+const logoImage = require('./assets/icon.png');
+
 export default class App extends React.Component {
-  state={};
+  state={
+    animatedRotateValue: new Animated.Value(0),
+    animatedScaleValue: new Animated.Value(0),
+  };
+
+  componentDidMount() {
+    const { animatedRotateValue, animatedScaleValue } = this.state;
+
+    Animated.timing(
+      animatedScaleValue, {
+        toValue: 1,
+        useNativeDriver: true,
+        duration: 3000,
+        easing: Easing.elastic(2),
+      },
+    ).start();
+
+    Animated.loop(
+      Animated.timing(
+        animatedRotateValue,
+        {
+          toValue: 1,
+          duration: 2000,
+          useNativeDriver: true,
+          easing: Easing.linear,
+        },
+      ),
+    ).start();
+  }
 
   render() {
+    const { animatedRotateValue, animatedScaleValue } = this.state;
+
+    const spinTransform = animatedRotateValue.interpolate({
+      inputRange: [0, 1],
+      outputRange: ['0deg', '360deg'],
+    });
+
+    const scaleTransform = animatedScaleValue.interpolate({
+      inputRange: [0, 1],
+      outputRange: [0, 1],
+    });
+
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Animated.Image
+          source={logoImage}
+          style={{
+            height: 100,
+            width: 100,
+            transform: [
+              { rotate: spinTransform },
+              { scaleX: scaleTransform },
+              { scaleY: scaleTransform },
+            ],
+          }}
+        />
+        <Text style={styles.t1}>
+          Checkout the branches of this repo to try out awesome animations.
+        </Text>
+        <Text style={styles.t2}>
+          {'😬 Also check README.md 😬'}
+        </Text>
       </View>
     );
   }
@@ -21,5 +82,18 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 32,
+  },
+  t1: {
+    fontWeight: '500',
+    textAlign: 'center',
+    marginTop: 16,
+  },
+  t2: {
+    fontWeight: '300',
+    textAlign: 'center',
+    marginTop: 16,
   },
 });
