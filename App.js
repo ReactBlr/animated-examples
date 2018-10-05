ee1a2ebaf821b2c5c38b5396f458746f1e0761be
Kashish Grover <kashish.grover@treebohotels.com>
2018-10-05 23:48:31 +0530

    add background animation example

--- App.js
+++ App.js
@@ -1,17 +1,47 @@
+/*
+  In this example, we have a Button and an Animated.View. Click the Button and
+  the View becomes opaque. Click it again and the View becomes transparent.
+
+  Take a close look at what happens. On click the Animated.Value will time to
+  the 'toValues' mention in the onPressButton function.
+
+  Try fiddling around with the 'toValues' and the styles of the Animated.View.
+*/
+
 import React from 'react';
-import { StyleSheet, View, Button } from 'react-native';
+import { StyleSheet, View, Button, Animated } from 'react-native';
 import { Constants } from 'expo';
 
 export default class App extends React.Component {
-  state={};
+  state={
+    valueA: new Animated.Value(0),
+    animatingIn: false,
+  };
 
   onPressButton = () => {
+    const { animatingIn, valueA } = this.state;
+    this.setState({ animatingIn: !animatingIn });
 
+    if (animatingIn) {
+      Animated.timing(valueA, { toValue: 0, duration: 1000 }).start();
+    } else {
+      Animated.timing(valueA, { toValue: 1, duration: 1000 }).start();
+    }
   }
 
   render() {
+    const { valueA } = this.state;
+
     return (
       <View style={styles.container}>
+        <Animated.View
+          style={{
+            opacity: valueA,
+            width: 200,
+            height: 200,
+            backgroundColor: '#ffaa00',
+          }}
+        />
         <Button
           title="Click Me"
           onPress={this.onPressButton}
@@ -28,5 +58,7 @@ const styles = StyleSheet.create({
     paddingTop: Constants.statusBarHeight,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  animatedViewStyle: {
   },
 });
